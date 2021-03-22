2c2799996b354bc2e810a8c0f9798da8535f17cd
Vasco Martins <vascomartins37@gmail.com>
2021-03-22 13:02:02 +0000

    Initial Commit

---
+++ Assets/Scripts/Controller/SettingsController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsController : MonoBehaviour
+{
+
+    public AudioMixer AudioMixer;
+
+    public float CurrentVolume { get; set; }
+
+    public Slider VolumeSlider { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Get Components
+        VolumeSlider = GameObject.Find("General Volume Slider").GetComponent<Slider>();
+
+        // Initialize Default Values
+        CurrentVolume = PlayerPrefs.GetFloat("Volume Preference");
+
+        VolumeSlider.value = CurrentVolume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMainVolume()
+    {
+        float value = VolumeSlider.value;
+        
+        Debug.Log("[SettingsController] Changing volume to" + value);
+
+        AudioMixer.SetFloat("Master Volume", value);
+        PlayerPrefs.SetFloat("Volume Preference", value);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(int scene)
+    {
+        Debug.Log("[UIManager] Scene Loaded -> " + scene);
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
