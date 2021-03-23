64366d2b09bedf3dea9257c8a42d5185e1453711
Vasco Martins <vascomartins37@gmail.com>
2021-03-23 18:29:18 +0000

    Refactor home screen

---
+++ Assets/Scripts/ButtonFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFX : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public AudioClip clickSound;
+    public AudioClip hoverSound;
+
+    public void HoverSound()
+    {
+        audioSource.PlayOneShot(hoverSound);
+    }
+
+
+    public void ClickSound()
+    {
+        audioSource.PlayOneShot(clickSound  );
+    }
+}
